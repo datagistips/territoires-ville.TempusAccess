400b74c63aed1de107d9070e678b9ed8cec532ca
Aurélie Bousquet <aurelie.bousquet@cerema.fr>
2019-01-14 19:57:43 +0100

    Enhance PT isochrons representation

--- scripts/tempusloader/provider/sql/reset_tempusaccess_function_paths_tree.sql
+++ scripts/tempusloader/provider/sql/reset_tempusaccess_function_paths_tree.sql
@@ -22,7 +22,7 @@ DECLARE
 
 BEGIN
     
-	-- Update of the temporary results table
+	-- Update nodes and edges geometries
 	UPDATE tempus_access.tempus_paths_tree_results 
 	SET x_from = pred.x, y_from = pred.y
 	FROM tempus_access.tempus_paths_tree_results pred
@@ -31,7 +31,8 @@ BEGIN
     UPDATE tempus_access.tempus_paths_tree_results
     SET geom_from = st_setsrid(st_makepoint(x_from, y_from), 4326),
         geom_to = st_setsrid(st_makepoint(x, y), 4326);
-        
+    
+    -- Update road nodes and sections
     WITH q AS
     (
 	SELECT tempus_paths_tree_results.gid, road_node.id
@@ -72,12 +73,55 @@ BEGIN
     WHERE tempus_paths_tree_results.transport_mode = transport_mode.id AND transport_mode.public_transport = FALSE 
       AND ((road_section.node_from = road_node_from AND road_section.node_to = road_node_to) OR (road_section.node_to = road_node_from AND road_section.node_from = road_node_to));
     
+    -- Update PT nodes and sections
+    WITH q AS
+    (
+	SELECT tempus_paths_tree_results.gid, stop.id
+	FROM tempus_access.tempus_paths_tree_results
+	CROSS JOIN LATERAL
+		(
+			SELECT id
+			FROM tempus_gtfs.stops
+			ORDER BY tempus_paths_tree_results.geom_from <-> stops.geom
+			LIMIT 1
+		) AS stop
+    )
+    UPDATE tempus_access.tempus_paths_tree_results
+    SET pt_node_from = q.id
+    FROM q
+    WHERE q.gid = tempus_paths_tree_results.gid;
+    
+    WITH q AS
+    (
+	SELECT tempus_paths_tree_results.gid, stop.id
+	FROM tempus_access.tempus_paths_tree_results
+	CROSS JOIN LATERAL
+		(
+			SELECT id
+			FROM tempus_gtfs.stops
+			ORDER BY tempus_paths_tree_results.geom_to <-> stops.geom
+			LIMIT 1
+		) AS stop
+    )
+    UPDATE tempus_access.tempus_paths_tree_results
+    SET pt_node_to = q.id
+    FROM q
+    WHERE q.gid = tempus_paths_tree_results.gid;
+    
+    UPDATE tempus_access.tempus_paths_tree_results
+    SET pt_section_id = sections.id
+    FROM tempus.transport_mode, tempus_gtfs.sections
+    WHERE tempus_paths_tree_results.transport_mode = transport_mode.id AND transport_mode.public_transport = FALSE 
+      AND ((sections.stop_from = pt_node_from AND sections.stop_to = pt_node_to) OR (sections.stop_to = ptnode_from AND sections.node_from = pt_node_to));
+    
+    -- Add facultative indicators
     indics_str = ''; 
     FOR r IN (SELECT col_name FROM tempus_access.indicators WHERE ARRAY[code] <@ param_indics)
     LOOP 
         indics_str = indics_str || (SELECT coalesce(replace(day_ag_paths_trees, '%(day_ag)', indics_str::character varying) || ' AS ' || r.col_name || ', ', '') FROM tempus_access.indicators WHERE col_name = r.col_name);         
     END LOOP;  
     
+    -- Create final table
 	s=$$
         DROP TABLE IF EXISTS indic.paths_tree;
         CREATE TABLE indic.paths_tree AS
@@ -89,10 +133,13 @@ BEGIN
                (total_cost::character varying || ' minutes')::interval::character varying AS d_time, 		
                ((total_cost - cost)::character varying || ' minutes')::interval::character varying AS o_time,    
                $$ || indics_str || $$
-               CASE WHEN road_section_id IS NOT NULL THEN road_section.geom END AS geom_section, 
+               CASE WHEN road_section_id IS NOT NULL THEN road_section.geom
+                    WHEN pt_section_id IS NOT NULL THEN sections.geom
+               END AS geom_section, 
                st_setsrid(st_makepoint(x, y), 4326) AS geom_point
             FROM tempus_access.tempus_paths_tree_results JOIN tempus.transport_mode ON (tempus_paths_tree_results.transport_mode = transport_mode.id)
-                                                         JOIN tempus.road_section ON (tempus_paths_tree_results.road_section_id = road_section.id) 
+                                                         LEFT JOIN tempus.road_section ON (tempus_paths_tree_results.road_section_id = road_section.id)
+                                                         LEFT JOIN tempus_gtfs.sections ON (tempus_paths_tree_results.pt_section_id = sections.id)
         );
         ALTER TABLE indic.paths_tree 
         ADD CONSTRAINT paths_tree_pkey PRIMARY KEY(d_node);
